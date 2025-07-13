856f000fb10f5170dcbe0f1b55e30f0a0fc0d498
madhvesh-77 <madhveshs@sahaj.ai>
2025-07-13 22:54:48 +0530

    Create Transaction Model

---
+++ src/main/java/com/example/personalFinance/model/Transaction.java
@@ -0,0 +1,46 @@
+package com.example.personalFinance.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "transaction_type", nullable = false)
+    private String transactionType;
+
+    @Column(name = "transaction_amount", nullable = false)
+    private Integer transactionAmount;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDate transactionDate;
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public Integer getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(Integer transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+}
